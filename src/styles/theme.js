2439d8430eb1c3258b90553e51b3651d60f9b07e
Reese Parker <parker.rhys@outlook.com>
2022-04-20 22:29:43 -0400

    Added styling to components

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: "#212a2f",
+    },
+  },
+  typography: {
+    fontFamily: ["Nanum Gothic", "sans-serif"].join(","),
+  },
+});
